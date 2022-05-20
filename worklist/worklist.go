98a1a78ec55a0de713c9210d0998887641d069ac
Carlo Munguia <cmunguia@denimgroup.com>
2022-05-20 15:28:21 -0500

    Grape 2
    Worklist structure fleshed out/created

--- worklist/worklist.go
+++ worklist/worklist.go
@@ -1,1 +1,32 @@
 package worklist
+
+type Entry struct {
+	Path string
+}
+
+type Worklist struct {
+	jobs chan Entry
+}
+
+func (w *Worklist) Add(work Entry) {
+	w.jobs <- work
+}
+
+func (w *Worklist) Next() Entry {
+	j := <-w.jobs
+	return j
+}
+
+func New(bufferSize int) Worklist {
+	return Worklist{make(chan Entry, bufferSize)}
+}
+
+func NewJob(path string) Entry {
+	return Entry{path}
+}
+
+func (w *Worklist) Finalize(numWorkers int) {
+	for i := 0; i < numWorkers; i++ {
+		w.Add(Entry{""})
+	}
+}
