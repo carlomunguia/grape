7e11572708f9b145c672ae92f56245674446e4f5
Carlo Munguia <cmunguia@denimgroup.com>
2022-05-21 12:37:56 -0500

    Grape 5
    recurse directory / worker path entry / chan thread creation, goroutines connecting

--- grape/main.go
+++ grape/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"grape/worker"
 	"grape/worklist"
 	"os"
 	"path/filepath"
+	"sync"
+
+	"github.com/alexflint/go-arg"
 )
 
 func discoverDirs(wl *worklist.Worklist, path string) {
@@ -26,4 +30,44 @@ func discoverDirs(wl *worklist.Worklist, path string) {
 var args struct {
 	SearchTerm string `arg:"positional, required"`
 	SearchDir  string `arg:"positional"`
+}
+
+func main() {
+	arg.MustParse(&args)
+
+	var workersWg sync.WaitGroup
+
+	wl := worklist.New(100)
+
+	results := make(chan worker.Result, 100)
+
+	numWorkers := 10
+
+	workersWg.Add(1)
+	go func() {
+		defer workersWg.Done()
+		discoverDirs(&wl, args.SearchDir)
+		wl.Finalize(numWorkers)
+	}()
+
+	for i := 0; i < numWorkers; i++ {
+		workersWg.Add(1)
+		go func() {
+			defer workersWg.Done()
+			for {
+				workEntry := wl.Next()
+				if workEntry.Path != "" {
+					workerResult := worker.FindInFile(workEntry.Path, args.SearchTerm)
+					if workerResult != nil {
+						for _, r := range workerResult.Inner {
+							results <- r
+						}
+					}
+				} else {
+					return
+				}
+			}
+		}()
+	}
+
 }
