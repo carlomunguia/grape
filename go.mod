21165b595339685bc6a3c6553886343434e64263
Carlo Munguia <cmunguia@denimgroup.com>
2022-05-20 22:02:10 -0500

    Grape 4
    created discovery method to handle channels/jobs, arg struct

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module grape
 
 go 1.18
+
+require github.com/alexflint/go-arg v1.4.3
+
+require github.com/alexflint/go-scalar v1.1.0 // indirect
--- grape/main.go
+++ grape/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"grape/worklist"
+	"os"
+	"path/filepath"
+)
 
+func discoverDirs(wl *worklist.Worklist, path string) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Println("Readdir error:", err)
+		return
+	}
+	for _, entry := range entries {
+		if entry.IsDir() {
+			nextPath := filepath.Join(path, entry.Name())
+			discoverDirs(wl, nextPath)
+		} else {
+			wl.Add(worklist.NewJob(filepath.Join(path, entry.Name())))
+		}
+	}
+}
+
+var args struct {
+	SearchTerm string `arg:"positional, required"`
+	SearchDir  string `arg:"positional"`
 }
